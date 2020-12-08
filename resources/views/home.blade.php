9e2d5da9002f054328617434cb97849fdd715026
ReubenSiama <reubenlalhmunsiama@gmail.com>
2020-12-08 20:58:01 +0530

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,9 +45,3 @@
     </div>
 </div>
 @endsection
-
-@section('script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="/assets/demo/chart-area-demo.js"></script>
-    <script src="/assets/demo/chart-bar-demo.js"></script>
-@endsection
